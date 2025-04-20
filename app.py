90ec4a2d62e2fcaae81f54f2b75ccc19e064a1ef
Srrt <113283637+Srrtx@users.noreply.github.com>
2025-04-20 16:42:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ y_kmeans = loaded_model.predict(X)
 #plotting
 fig, ax = plt.subplots()
 scatter = ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
-ax.scatter(loaded_model.cluster_centers_[:, 0], loaded_model.cluster_centers_[:, 1], s=300, c='red')
+ax.scatter(loaded_model.cluster_centers_[:, 0], loaded_model.cluster_centers_[:, 1], s=300, c='red',label='Centroids')
 ax.set_title('k-Means Clustering')
-ax.set_legend()
+ax.legend()
 st.pyplot(fig)
 #Display
 
